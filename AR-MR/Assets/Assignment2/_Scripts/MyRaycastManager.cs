d516bb99f2aee6384ac301e22511d65bcfcac3d1
Santiago Sala <pipo0716@gmail.com>
2022-02-17 01:26:05 +0200

    Managed to spawn an object on where the cast hits the plane

--- AR-MR/Assets/Assignment2/_Scripts/MyRaycastManager.cs
+++ AR-MR/Assets/Assignment2/_Scripts/MyRaycastManager.cs
@@ -11,6 +11,10 @@ public class MyRaycastManager : MonoBehaviour
     [SerializeField] private TMP_Text _uiText;
     [SerializeField] private TMP_Text _markText;
     [SerializeField] private GameObject _hitMark;
+    [SerializeField] private GameObject _spawnPivot;
+
+    private bool _markActive = false;
+    [SerializeField] private GameObject _spawns;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,12 +38,25 @@ public class MyRaycastManager : MonoBehaviour
             _uiText.text = hits[0].trackable.transform.ToString();
             _hitMark.transform.position = new Vector3(hits[0].trackable.transform.position.x, hits[0].trackable.transform.position.y, hits[0].trackable.transform.position.z);
             _markText.text = _hitMark.transform.position.ToString();
+            _markActive = true;
         }
         else
         {
             _uiText.text = "NO hits man!!";
             _hitMark.SetActive(false);
+            _markActive = false;
         }
         
     }
-}
+    public void SpawnObject()
+    {
+        if (_markActive)
+        {
+            Instantiate(_spawns, _hitMark.transform.position, _hitMark.transform.rotation, _spawnPivot.transform );
+        }
+        else
+        {
+            return;
+        }
+    }
+} 
