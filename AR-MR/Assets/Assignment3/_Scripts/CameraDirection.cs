f836ea5ce7bc9d9955890a7d03cf481e7a7dc72e
Santiago Sala <pipo0716@gmail.com>
2022-04-22 11:33:29 +0300

    Implemented changing the cameras

---
+++ AR-MR/Assets/Assignment3/_Scripts/CameraDirection.cs
@@ -0,0 +1,36 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class CameraDirection : MonoBehaviour
+{
+    [SerializeField]
+    private ARCameraManager _cameraView;
+    [SerializeField]
+    public TMP_Text _toggleCameraText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraView = GetComponent<ARCameraManager>();
+    }
+
+    public void ChangeCameraDirection()
+    {
+        //Debug.Log("Current view: " + _cameraView.currentFacingDirection.ToString());
+        if (_cameraView.requestedFacingDirection == CameraFacingDirection.User)
+        {
+            _cameraView.requestedFacingDirection = CameraFacingDirection.World;
+            //Debug.Log("To User " + _cameraView.currentFacingDirection.ToString());
+
+            _toggleCameraText.text = "To REAR Camera";
+           
+        }
+        else
+        {
+            _cameraView.requestedFacingDirection = CameraFacingDirection.User;
+            //Debug.Log("To rear " + _cameraView.currentFacingDirection.ToString());
+            
+            _toggleCameraText.text = "To FRONT Camera";
+        }
+    }
+}
