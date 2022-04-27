b5b213625c2e2d50dcd7545fa18e3ba268e21e02
Santiago Sala <pipo0716@gmail.com>
2022-04-27 15:13:31 +0300

    So far so good

--- AR-MR/Assets/Assignment3_ImageTracking/_Scripts/SpawnLogo.cs
+++ AR-MR/Assets/Assignment3_ImageTracking/_Scripts/SpawnLogo.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class SpawnLogo : MonoBehaviour
 {
@@ -23,6 +24,8 @@ public class SpawnLogo : MonoBehaviour
     private GameObject[] _prefabsToBeSpawned;
     [SerializeField]
     private Dictionary<string, GameObject> _spawnedPrefabs = new Dictionary<string, GameObject>();
+
+    private bool _trackedImage = false;
 
     private void Awake()
     {
@@ -47,47 +50,98 @@ public class SpawnLogo : MonoBehaviour
     }
 
     void Update()
-    {       
-        if (_imageManager.trackables.count <= 0)
+    {
+        if (_imageManager.trackables.count > 0)
         {
-            _visitWebSitePanel.SetActive(false);
-            _scanMode.SetActive(true);
+            foreach (var image in _imageManager.trackables)
+            {
+                if (image.trackingState == TrackingState.Tracking)
+                {
+                    _trackedImage = true;
+                }
+                else
+                {
+                    _trackedImage = false;
+                }
+            }
         }
-        else
+
+        if (_trackedImage)
         {
             _scanMode.SetActive(false);
             _visitWebSitePanel.SetActive(true);
         }
-        
+        else
+        {
+            if (_spawnedPrefabs.Values != null)
+            {
+                foreach (var spawnedPrefab in _spawnedPrefabs.Values)
+                {
+                    spawnedPrefab.SetActive(false);
+                }
+            }
+            _visitWebSitePanel.SetActive(false);
+            _scanMode.SetActive(true);
+
+        }
+        //if (_imageManager.trackables.count <= 0)
+        //{
+        //    _visitWebSitePanel.SetActive(false);
+        //    _scanMode.SetActive(true);
+        //}
+        //else
+        //{
+        //    _scanMode.SetActive(false);
+        //    _visitWebSitePanel.SetActive(true);
+        //}
+
     }
 
     private void ImageChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
         foreach (ARTrackedImage trackedImage in eventArgs.added)
         {
+            Debug.Log("ADDED Started");
             InstantiateImage(trackedImage);
+            Debug.Log("ADDED Ended");
         }
 
         foreach (ARTrackedImage trackedImage in eventArgs.updated)
         {
+            Debug.Log("UPDATED Started");
             InstantiateImage(trackedImage);
-           
+            Debug.Log("UPDATED Ended");
+
         }
 
         foreach (ARTrackedImage trackedImage in eventArgs.removed)
         {
+            Debug.Log("REMOVED Started");
             _spawnedPrefabs[trackedImage.name].SetActive(false);
             _visitWebSitePanel.SetActive(false);
             _scanMode.SetActive(true);
-           
+            Debug.Log("REMOVED Ended");
+
         }
     }
 
     private void InstantiateImage(ARTrackedImage image)
-    {
+    {   
         string name = image.referenceImage.name;
         Vector3 imagePosition = image.transform.position;
 
+        GameObject prefab = _spawnedPrefabs[name];
+        prefab.transform.position = imagePosition;
+        prefab.SetActive(true);        
+
+        foreach (GameObject go in _spawnedPrefabs.Values)
+        {
+            if (go.name != name)
+            {
+                go.SetActive(false);
+            }
+        }
+        
         switch (name)
         {
             case "JAMK_LOGO":
@@ -101,18 +155,7 @@ public class SpawnLogo : MonoBehaviour
             default:
                 break;
         }
-        GameObject prefab = _spawnedPrefabs[name];
-        prefab.transform.position = imagePosition;
-        prefab.SetActive(true);
-        
-
-        foreach (GameObject go in _spawnedPrefabs.Values)
-        {
-            if (go.name != name)
-            {
-                go.SetActive(false);
-            }
-        }
+                
     }
 
     public void OpenWebsite()
@@ -120,4 +163,32 @@ public class SpawnLogo : MonoBehaviour
         Application.OpenURL(_url);
     }
 
+    //it  just doesnt work i cannot manage to remove my image trackables to zero.
+    //public void BackToScanMode()
+    //{
+    //    //Debug.Log("Kpasa 1: " + _imageManager.trackables.count.ToString());
+    //    //foreach (var trackedImage in _imageManager.trackables)
+    //    //{
+    //    //    Debug.Log("NOSE: " + _imageManager.trackables.count.ToString());
+    //    //    //trackedImage.gameObject.SetActive(false);
+    //    //    trackedImage.destroyOnRemoval = true;
+    //    //    //trackedImage.enabled = false;
+    //    //    Destroy(trackedImage);
+    //    //    Debug.Log("NOSE 1****: " + _imageManager.trackables.count.ToString());
+    //    //}
+
+    //    foreach (var spawnedPrefab in _spawnedPrefabs.Values)
+    //    {
+    //        spawnedPrefab.SetActive(false );
+    //    }
+    //    //Debug.Log("Kpasa 1****: " + _imageManager.trackables.count.ToString());
+
+    //    _btnText.text = "";
+    //    _url = "";
+    //    _visitWebSitePanel.SetActive(false);
+    //    _scanMode.SetActive(true);
+
+
+
+    //}
 }
